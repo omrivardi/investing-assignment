b78c7ca0bedeaf02ccd518a12240569f46c4167f
omrivard <omrivardi@gmail.com>
2020-05-16 17:32:11 +0300

    remove db folder

--- db/initDb.js
+++
@@ -1,9 +0,0 @@
-const { promisify } = require("util");
-const Datastore = require("nedb");
-const allInstruments = require("./data/instruments.json");
-
-const db = {};
-db.instruments = new Datastore();
-const insertAsync = promisify(db.instruments.insert);
-allInstruments.map((instrument) => db.instruments.insert());
-const db = new Datastore({ filename: ".data/list", autoload: true });
